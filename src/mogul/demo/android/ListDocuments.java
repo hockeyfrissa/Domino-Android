93f882596cc5e8e175b602759edec4f744fd32b4
Martin <hockeyfrissa@gmail.com>
2011-09-21 13:39:24 +0200

    First

---
+++ src/mogul/demo/android/ListDocuments.java
@@ -0,0 +1,13 @@
+package mogul.demo.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListDocuments extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
