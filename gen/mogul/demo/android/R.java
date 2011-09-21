e99c3d4c1093f4748a80914ddda4b2a0dd1ae611
Martin <hockeyfrissa@gmail.com>
2011-09-21 20:12:53 +0200

    ListView

--- gen/mogul/demo/android/R.java
+++ gen/mogul/demo/android/R.java
@@ -8,13 +8,21 @@
 package mogul.demo.android;
 
 public final class R {
+    public static final class array {
+        public static final int countries_array=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int listView1=0x7f060001;
+        public static final int textView1=0x7f060000;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int list_item=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
